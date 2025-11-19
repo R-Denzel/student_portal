a13762f9aa233d4e19102d73c1853c3f318adee7
R-Denzel <robertdenzel5@gmail.com>
2025-11-19 16:39:37 +0300

    Fixed the settings.py and dashboard

--- student_portal/student_portal/settings.py
+++ student_portal/student_portal/settings.py
@@ -55,10 +55,11 @@ ROOT_URLCONF = "student_portal.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR/ 'templates'],
+        "DIRS": [BASE_DIR / 'student_portal' / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
